017990c261711182fb8f3893070c90d508804085
biavctr <93948390+biavctr@users.noreply.github.com>
2022-12-17 16:56:06 +0000

    Update queries.sql

--- Final files/queries.sql
+++ Final files/queries.sql
@@ -20,6 +20,8 @@ left join catalog_items ci
 on cti.item_id = ci.item_id
 where o.date_order between '2022-01-01' and '2022-06-01';
 
+-- Here we listed all the names, orders and dates of the costumers and the filtered by the requested range of dates.
+
 
 
 
@@ -42,6 +44,10 @@ group by 1,2,3
 order by total_orders desc
 limit 3;
 
+-- In this query we listed all the customers and their total amount of purchases, then we ordered descendently and limit to only 3 customers, 
+-- that way the query will only return the top 3 customers by number of orders.
+
+
 -- 3. Get the average amount of sales/bookings/rents/deliveries for a period that involves 2 or more years, as in the following example. This query only returns one record.
 
 select 
@@ -55,6 +61,8 @@ on c.cart_id = o.cart_id
 where date_order between '2020-01-01' and '2023-01-30'
 group by 1
 ;
+-- Here we created a new column to show what is the period we are refering to, then we summed the data and divided by the period requestd in months.
+-- To finalize where filtered by the date and group by the date column wue created.
 
 -- 4. Get the total sales/bookings/rents/deliveries by geographical location (city/country).
 select 
@@ -70,7 +78,7 @@ on s.session_id = c.session_id
 left join orders o 
 on c.cart_id = o.cart_id
 group by 1,2,3;
-
+-- Here we calculated the sum of spent money and grouped by the places requested on the query.
 
 -- 5. List all the locations where products/services were sold, and the product has customer’s ratings
 -- (Yes, your ERD must consider that customers can give ratings)
@@ -90,6 +98,6 @@ left join orders o
 on c.cart_id = o.cart_id
 left join ratings r
 on o.order_id = r.order_id
-where r.rating is not null
-and o.order_id is not null
-limit 50;
+where r.rating is not null -- filtering only orders with ratings
+and o.order_id is not null -- filtering only places with orders
+limit 50; --Our database it too large, that's why we limited in only 50 rows
